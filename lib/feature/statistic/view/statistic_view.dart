42253a84ebd2c52ffa524d54e499b996f6bceb7a
burakJs <burakimdat13@gmail.com>
2022-03-22 21:26:47 +0300

    feat: add balance and calendar row

---
+++ lib/feature/statistic/view/statistic_view.dart
@@ -0,0 +1,116 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
+import 'package:pockeet/core/constants/color_constants.dart';
+import 'package:pockeet/core/init/lang/locale_keys.g.dart';
+
+class StatisticView extends StatelessWidget {
+  const StatisticView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorConstants colors = ColorConstants.instance;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: colors.primaryPurpleColor,
+        title: Text(LocaleKeys.appBar_title_statistic.tr()),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(color: context.randomColor),
+            ),
+            Expanded(
+              child: Padding(
+                padding: context.paddingNormal,
+                child: _balanceAndCalendarRow(context, colors),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(color: context.randomColor),
+            ),
+            Expanded(
+              child: Container(color: context.randomColor),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row _balanceAndCalendarRow(BuildContext context, ColorConstants colors) {
+    return Row(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _totalBalanceText(context),
+            const Spacer(),
+            _balanceText(context, colors),
+            const Spacer(),
+          ],
+        ),
+        const Spacer(),
+        _calendarBackgroundContainer(colors, context),
+      ],
+    );
+  }
+
+  Text _totalBalanceText(BuildContext context) {
+    return Text(
+      LocaleKeys.statistic_totalBalance.tr(),
+      style: context.textTheme.titleMedium,
+    );
+  }
+
+  Text _balanceText(BuildContext context, ColorConstants colors) {
+    return Text(
+      '\$687.134',
+      style: context.textTheme.headline4?.copyWith(
+        color: colors.yellowColor,
+        fontWeight: FontWeight.bold,
+        fontSize: 27,
+      ),
+    );
+  }
+
+  Container _calendarBackgroundContainer(ColorConstants colors, BuildContext context) {
+    return Container(
+      color: colors.blackCardBackgroundColor,
+      child: Padding(
+        padding: context.paddingLow,
+        child: Row(
+          children: [_calendarText(context), context.emptySizedWidthBoxNormal, _calendarContainer(context, colors)],
+        ),
+      ),
+    );
+  }
+
+  Text _calendarText(BuildContext context) {
+    return Text(
+      'Jan, 2022',
+      style: context.textTheme.titleMedium?.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 21,
+      ),
+    );
+  }
+
+  Container _calendarContainer(BuildContext context, ColorConstants colors) {
+    return Container(
+      width: context.dynamicWidth(0.11),
+      height: context.dynamicWidth(0.11),
+      decoration: BoxDecoration(
+        color: colors.primaryPurpleColor,
+        borderRadius: context.lowBorderRadius,
+      ),
+      child: IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.calendar_today),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:pockeet/core/init/navigation/concrete/navigation_manager.dart';
 import 'package:pockeet/core/init/navigation/concrete/navigation_route.dart';
+import 'package:pockeet/feature/statistic/view/statistic_view.dart';
 
 import 'core/constants/app_constants.dart';
 import 'core/init/langugae/language_manager.dart';
@@ -32,16 +33,7 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       theme: ThemeManager.createTheme(AppDarkTheme()),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: const StatisticView(),
     );
   }
 }
