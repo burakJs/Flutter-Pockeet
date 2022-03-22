e1c2c58310fea9119ce8aa3c49616ae3870a5f9f
resulbdev <resulbozdemirr@gmail.com>
2022-03-22 14:39:47 +0300

    feat: add app bar

--- lib/feature/homepage/home_view.dart
+++ lib/feature/homepage/home_view.dart
@@ -6,37 +6,54 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
+      backgroundColor: const Color(0xFF121127),
       body: Padding(
         padding: const EdgeInsets.only(left: 24, right: 24, top: 42),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                const CircleAvatar(
-                  backgroundColor: Colors.purple,
-                  child: Center(
-                    child: Text("D"),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const CircleAvatar(
+                    backgroundColor: Colors.purple,
+                    child: Center(
+                      child: Text("D"),
+                    ),
                   ),
-                ),
-                Column(
-                  children: const [
-                    Text("Welcome Back!"),
-                    Text("Devon Albexx"),
-                  ],
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                ),
-                Container(
-                  width: 4,
-                  height: 4,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(4))),
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  Column(
+                    children: const [
+                      Text(
+                        "Welcome Back!",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "Devon Albex",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  Container(
+                    width: 40,
+                    height: 40,
+                    child:
+                        Center(child: Image.asset("assets/icons/Profile.png")),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Color(0xFF1f1e34),
+                    ),
+                  ),
+                ],
+              ),
+              ListTile(),
+            ],
+          ),
         ),
       ),
     );
