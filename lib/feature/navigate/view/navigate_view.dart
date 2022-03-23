b5047d5212637c57004ab52ac80623d20938da74
burakJs <burakimdat13@gmail.com>
2022-03-23 14:20:26 +0300

    feat: add navigate with bottom navigation bar

---
+++ lib/feature/navigate/view/navigate_view.dart
@@ -0,0 +1,48 @@
+import 'package:fluid_bottom_nav_bar/fluid_bottom_nav_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pockeet/core/constants/color_constants.dart';
+
+import '../viewmodel/navigate_bloc.dart';
+
+class NavigateView extends StatelessWidget {
+  NavigateView({Key? key}) : super(key: key);
+
+  final ColorConstants _colors = ColorConstants.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: FluidNavBar(
+        defaultIndex: 0,
+        onChange: (int val) => context.read<NavigateBloc>().changePage(val),
+        style: FluidNavBarStyle(
+          iconSelectedForegroundColor: _colors.primaryPurpleColor,
+          barBackgroundColor: _colors.backgroundColor,
+        ),
+        icons: [
+          FluidNavBarIcon(
+            icon: Icons.home,
+          ),
+          FluidNavBarIcon(
+            icon: Icons.document_scanner,
+          ),
+          FluidNavBarIcon(
+            icon: Icons.add,
+          ),
+          FluidNavBarIcon(
+            icon: Icons.graphic_eq,
+          ),
+          FluidNavBarIcon(
+            icon: Icons.person,
+          ),
+        ],
+      ),
+      body: BlocBuilder<NavigateBloc, int>(
+        builder: (context, state) {
+          return context.read<NavigateBloc>().pages[state];
+        },
+      ),
+    );
+  }
+}
